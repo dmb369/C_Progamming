8cb25d044b630868e8b6059b21b5c2c2a2f78a09
dmb369 <116587999+dmb369@users.noreply.github.com>
2023-02-05 22:24:57 +0530

    Add files via upload

---
+++ Alphabets.c
@@ -0,0 +1,14 @@
+// C program to print all the alphabets.
+#include <stdio.h>
+
+int main(void) {
+    int i='a';
+    
+    while (i<='z')
+    {
+        printf("%c ",i);
+        i++;
+    }
+
+    return 0;
+}
